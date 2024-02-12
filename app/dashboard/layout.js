a2b2cddb0ad2213aeb30dc07f056b3d295a337b2
Rishabh <rishabharora95.ra@gmail.com>
2024-02-12 21:09:54 +0530

    Initial setup

---
+++ app/dashboard/layout.js
@@ -0,0 +1,9 @@
+import SideNav from "@/components/SideNav";
+
+export default function Layout({ children }) {
+    return (
+        <div>
+            <SideNav />
+        </div>
+    );
+}
---
+++ components/SideNav.jsx
@@ -0,0 +1,7 @@
+const SideNav = () => {
+    return (
+        <div>Side Nav</div>
+    );
+}
+
+export default SideNav;
