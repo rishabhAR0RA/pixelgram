20add4ca092b0e89e4ec22e2cbf47d57b3b7e583
Rishabh <rishabharora95.ra@gmail.com>
2024-02-20 19:22:07 +0530

    Login form and working google authentication

---
+++ app/(auth)/layout.jsx
@@ -0,0 +1,7 @@
+function AuthLayout({ children }) {
+    return (
+        <div className="grid place-items-center h-screen">{children}</div>
+    )
+}
+
+export default AuthLayout;
---
+++ app/(auth)/login/page.jsx
@@ -0,0 +1,11 @@
+import LoginForm from "@/components/forms/LoginForm";
+
+const Page = () => {
+    return (
+        <div>
+            <LoginForm />
+        </div>
+    );
+}
+
+export default Page;
