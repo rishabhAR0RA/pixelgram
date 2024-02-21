ea2ca8620ac369a48ca904e5f74f37d0cb46badc
Rishabh <rishabharora95.ra@gmail.com>
2024-02-21 21:20:29 +0530

    React-hook-form, zod setup

--- app/dashboard/create/page.jsx
+++ app/dashboard/create/page.jsx
@@ -20,6 +20,7 @@ import {
 } from "@/components/ui/form";
 import { Input } from "@/components/ui/input";
 import useMount from "@/hooks/useMount";
+import { CreatePost } from "@/lib/schema";
 // import { createPost } from "@/lib/actions";
 // import { CreatePost } from "@/lib/schemas";
 // import { UploadButton } from "@/lib/uploadthing";
@@ -37,6 +38,15 @@ const Page = () => {
     const isCreatePage = pathname === "/dashboard/create";
     const router = useRouter();
     const mount = useMount();
+    const form = useForm({
+        resolver: zodResolver(CreatePost),
+        defaultValues: {
+            caption: "",
+            fileUrl: undefined,
+        },
+    });
+
+    const fileUrl = form.watch("fileUrl");
 
     if (!mount) return null;
 
@@ -53,7 +63,53 @@ const Page = () => {
                         </DialogTitle>
                     </DialogHeader>
 
-
+                    <Form
+                        {...form}
+                    >
+                        <form className="space-y-4">
+                            {
+                                !!fileUrl ? (
+                                    <div className="h-96 md:h-[450px] overflow-hidden rounded-md">
+                                        <AspectRatio ratio={1 / 1} className="relative h-full">
+                                            <Image
+                                                src={fileUrl}
+                                                alt="Image preview"
+                                                fill
+                                                className="rounded-md object-cover"
+                                            />
+                                        </AspectRatio>
+                                    </div>
+                                ) : (
+                                    <FormField
+                                        control={form.control}
+                                        name="fileUrl"
+                                        render={({ field, fieldState }) => (
+                                            <FormItem>
+                                                <FormLabel htmlFor="picture">Picture</FormLabel>
+                                                <FormControl>
+                                                    {/* <UploadButton
+                                                        endpoint="imageUploader"
+                                                        onClientUploadComplete={(res) => {
+                                                            form.setValue("fileUrl", res[0].url);
+                                                            toast.success("Upload complete");
+                                                        }}
+                                                        onUploadError={(error: Error) => {
+                                                            console.error(error);
+                                                            toast.error("Upload failed");
+                                                        }}
+                                                    /> */}
+                                                </FormControl>
+                                                <FormDescription>
+                                                    Upload a picture to post.
+                                                </FormDescription>
+                                                <FormMessage />
+                                            </FormItem>
+                                        )}
+                                    />
+                                )
+                            }
+                        </form>
+                    </Form>
                 </DialogContent>
             </Dialog>
         </div>
